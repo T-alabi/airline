32737a37c044920720e9a5afe1d56f682c5809f0
Josephine Deng <jojod_27@yahoo.com>
2019-12-23 20:05:18 -0500

    started book form

---
+++ script.js
@@ -0,0 +1,9 @@
+function roundtrip() {
+  document.getElementById("return").style.display = "block";
+  document.getElementById("date2").style.display = "block";
+}
+
+function oneway() {
+  document.getElementById("return").style.display = "none";
+  document.getElementById("date2").style.display = "none";
+}
